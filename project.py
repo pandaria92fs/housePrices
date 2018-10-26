cf2d52221cb01d5412734116603df0cce144600f
pandaria92fs <xianhuipanpan@gmail.com>
2018-10-26 16:26:25 -0700

    add new

---
+++ project.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+
+
+
+
+
